b8ec5b60df4210809ef994e2fb9495eea990f9e8
Allen Arnoldy <allenarnoldy@Mt-Doom.local>
2024-09-30 17:01:17 -0500

    this gets to 90%

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -32,19 +32,25 @@ if (savedMode === 'dark') {
 }
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
+let blogData = [];
+
+readLocalStorage();
+// Creates a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
 function readLocalStorage() {
-  const blogData = JSON.parse(localStorage.getItem('blogData'));
-  if (!Array.isArray(blogData)) {
-    return [];
+  const storedData = JSON.parse(localStorage.getItem('blogData'));
+  if (storedData) {
+    blogData = storedData;
+  return blogData;
   } else {
+    blogData = [];
     return blogData;
   }
 }
-
-// TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
-function storeLocalStorage(blogData) {
+// Creates a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
+function storeLocalStorage() {
   localStorage.setItem('blogData', JSON.stringify(blogData));
 }
+
 
 // ! Use the following function whenever you need to redirect to a different page
 let redirectURL = '';
